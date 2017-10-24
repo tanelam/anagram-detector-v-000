b369a88f7ae2e1550594577d1df2d0cb99d6ee53
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-24 19:52:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+   words.select { |word| word.split("").sort.join == self.word.split("").sort.join }
+  end
+
+end
